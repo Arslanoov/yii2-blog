9571c16a9e82faa021651f82a79759125ece2b7a
Расуль Арсланов <rasularslanoov@gmail.com>
2019-10-01 07:28:45 +0500

    Installed Redis server

--- vagrant/provision/always-as-root.sh
+++ vagrant/provision/always-as-root.sh
@@ -10,3 +10,4 @@ info "Restart web-stack"
 service php7.2-fpm restart
 service nginx restart
 service mysql restart
+service redis restart
--- vagrant/provision/once-as-root.sh
+++ vagrant/provision/once-as-root.sh
@@ -30,6 +30,9 @@ apt-get upgrade -y
 info "Install additional software"
 apt-get install -y php7.2-curl php7.2-cli php7.2-intl php7.2-mysqlnd php7.2-gd php7.2-fpm php7.2-mbstring php7.2-xml unzip nginx mysql-server-5.7 php.xdebug
 
+info "Install Redis"
+apt-get install -y redis-server
+
 info "Configure MySQL"
 sed -i "s/.*bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
 mysql -uroot <<< "CREATE USER 'root'@'%' IDENTIFIED BY ''"
